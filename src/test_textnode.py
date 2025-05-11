0144eb9136925c1ac3c5c71314eba10c3f4623e8
taylor-ken <kenneth.n.taylor@gmail.com>
2025-05-11 14:56:55 -0400

    static site continues

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType, text_node_to_html_node
+from inline_markdown import extract_markdown_links, extract_markdown_images
 
 
 class TestTextNode(unittest.TestCase):
@@ -53,6 +54,12 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is bold")
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
 
 if __name__ == "__main__":
