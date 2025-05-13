16f2db953b989ad9d15450e489c8bdc9ddf335ad
taylor-ken <kenneth.n.taylor@gmail.com>
2025-05-13 16:39:35 -0400

    static site continues

--- src/main.py
+++ src/main.py
@@ -1,35 +1,20 @@
-from textnode import TextNode, TextType
 import os
 import shutil
 
-def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
-    current_path = os.getcwd()
-    parent_dir, current_folder = os.path.split(current_path)
-    source = os.path.join(parent_dir, 'static')
-    destination = os.path.join(parent_dir, 'public')
-    copy_static(source, destination)
+from copystatic import copy_files_recursive
 
-def copy_static(source, destination):
-    if os.path.exists(destination):
-        try:
-            shutil.rmtree(public_path)
-            print(f"Directory {public_path} has been removed successfully")
-        except OSError as error:
-            print(error)
-            print(f"Directory {public_path} can not be removed")
-    os.mkdir(destination)
-    contents_to_copy = os.listdir(source)
-    for item in contents_to_copy:
-        if os.path.isfile(item):
-            src = os.path.join(source, item)
-            dst = os.path.join(destination, item)
-            shutil.copy(src, dst)
-        if os.path.isdir(item):
-            src = os.path.join(source, item)
-            dst = os.path.join(destination, item)
-            copy_static(source, destination)
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def main():
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 main()
